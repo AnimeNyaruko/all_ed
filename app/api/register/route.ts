142e9940dbbb217da26d6d47543d2ddb4deb2d38
AnimeNyaruko <robloxandminecraft1603@gmail.com>
2025-04-16 11:45:05 +0700

    hotfix: Fix register page for new user to register

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -10,14 +10,18 @@ export async function POST(req: Request) {
 	const username = data.get("username")!.toString();
 	const password = await bcrypt.hash(data.get("password")!.toString(), 10);
 
-	const result = (
-		await sql`SELECT "Password" from "User Infomation"."Infomation" WHERE "Email" = ${email} LIMIT 1`
-	)[0].Password;
-	if (result === "") {
-		await sql`UPDATE "User Infomation"."Infomation" SET "Password" = ${password} WHERE "Email" = ${email}`;
-	} else if (result) {
-		return NextResponse.json("Tài khoản đã tồn tại!");
+	try {
+		const result = (
+			await sql`SELECT "Password" from "User Infomation"."Infomation" WHERE "Email" = ${email} LIMIT 1`
+		)[0].Password;
+
+		if (result === "") {
+			await sql`UPDATE "User Infomation"."Infomation" SET "Password" = ${password} WHERE "Email" = ${email}`;
+		} else if (result) {
+			return NextResponse.json("Tài khoản đã tồn tại!");
+		}
+	} catch (_error) {
+		await sql`INSERT INTO "User Infomation"."Infomation" ("Name", "Username", "Password", "Email") VALUES (${email},${username},${password},${email})`;
 	}
-	await sql`INSERT INTO "User Infomation"."Infomation" ("Name", "Username", "Password", "Email") VALUES (${email},${username},${password},${email})`;
 	return NextResponse.json("");
 }
